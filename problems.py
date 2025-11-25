d9521ec20239ae4f8d894c659398fd29acdd3990
jnbachi <284510@vutbr.cz>
2025-11-25 17:03:14 +0100

    problems.py

--- problems.py
+++ problems.py
@@ -2,12 +2,12 @@ import time
 import machine
 
 class ERRORS:
-  def _init_(self, i2c):
+  def __init__(self, i2c):
     self.i2c=i2c
 
   def i2c_check(self):
-    addrs= i2c.scan()
-    if (hex(addrs[1])= 0x5c && hex(addrs[0])=0x60) || (hex(addrs[1])= 0x60 && hex(addrs[0]=0x5c)):
+    addrs= self.i2c.scan()
+    if (addrs[1]== 0x5c and addrs[0]==0x60) or (addrs[1]== 0x60 and addrs[0]==0x5c):
       print("All the sensors are connected")
       return 1
     else:
