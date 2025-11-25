17d39f559abf66a30e9e2e1a4b50f80a10b8c99e
jnbachi <284510@vutbr.cz>
2025-11-25 14:07:57 +0100

    PossibleErrors.py

--- CheckConnection.py
+++ CheckConnection.py
@@ -1,3 +1,16 @@
 import time
+import machine
 
-def 
+class Errors:
+  def _init_(self, i2c):
+    self.i2c=i2c
+
+  def I2C_Check(self):
+    adrrs= i2c.scan()
+    if (adrrs[1]= 0x5c && adrrs[0]=0x60) || (adrrs[1]= 0x60 && adrrs[0]=0x5c):
+      print("All the sensors are connected")
+      return 1
+    else:
+      print("Problem Sensors. CHECK CONNECTION")
+      return 0
+      
