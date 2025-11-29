6d6f21fb196b5919f537fb5526898c2b8435204e
JulenArtola02 <278804@vut.cz>
2025-11-29 16:18:39 +0100

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import time
 import os
-from machine import I2C, Pin
+import machine
+from machine import I2C , Pin
 import dht12
 import network
 import ntptime
 import tools
+import sdcard
 try:
     #Constants
     ADDR = 0x60
@@ -16,22 +18,21 @@ try:
     PASSWORD="TomFryza"
     
     #SPI setup card
-    #spi = machine.SPI(1,
-    #baudrate=1000000,
-    #polarity=0,
-    #phase=0,
-    #sck=machine.Pin(18),
-    #mosi=machine.Pin(23),
-    #miso=machine.Pin(19)
-    #)
+    spi = machine.SPI(1,
+    baudrate=100000,
+    polarity=0,
+    phase=0,
+    sck=machine.Pin(18),
+    mosi=machine.Pin(23),
+    miso=machine.Pin(19)
+    )
     #chip select
-    #cs = machine.Pin(5, machine.Pin.OUT)
+    cs = machine.Pin(5, machine.Pin.OUT)
     
     #Initialize
-    #sd = machine.SDCard(spi=spi, cs=cs)
-    
-    #Create sd
-    #os.mount(sd, "/sd")
+    sd = sdcard.SDCard(spi, cs)
+    vfs = os.VfsFat(sd)
+    os.mount(vfs, "/sd")
     
     #I2C Setup
     i2c =I2C(0, scl=Pin(22), sda=Pin(21), freq=100_000)
@@ -67,40 +68,41 @@ try:
 
     
     #Prepare storage
-    with open("monitoring.csv", "a") as storage:
- 
-        # Monitorize
-        while True:
-            
-            #DHT12 section
-            
-            sensorTemHum.measure()
-            t=sensorTemHum.temperature()
-            h=sensorTemHum.humidity()
-            
-            #mpl3115a2 Section
-            
-            pressure_pa=0
-            status = i2c.readfrom_mem(ADDR, STATUS, 1)
-            if status[0] & 0x08:
-                data = i2c.readfrom_mem(ADDR, OUT_P_MSB, 3)
-                pres_raw = (data[0] << 16 | data[1] << 8 | data[2]) >> 6
-                pressure_pa = pres_raw / 4.0
-            else:
-                print("error altimeter")
-                
-                
-            local_time = time.localtime()
-                
-            print(f"Temperature: {t} C, Humidity: {h} RH, Relative Pressure: {pressure_pa:.2f} Pa, Year:{local_time[0]} , Month:{local_time[1]} , Day:{local_time[2]} , Hour:{local_time[3]}, Minute:{local_time[4]}, Second:{local_time[5]}" )
-            time.sleep(1)
+    storageI = open("monitoring.csv", "a")
+    storageE = open("/sd/monitoring.csv","a")
+    
+    # Monitorize
+    while True:
+        
+        #DHT12 section
+        
+        sensorTemHum.measure()
+        t=sensorTemHum.temperature()
+        h=sensorTemHum.humidity()
+        
+        #mpl3115a2 Section
+        
+        pressure_pa=0
+        status = i2c.readfrom_mem(ADDR, STATUS, 1)
+        if status[0] & 0x08:
+            data = i2c.readfrom_mem(ADDR, OUT_P_MSB, 3)
+            pres_raw = (data[0] << 16 | data[1] << 8 | data[2]) >> 6
+            pressure_pa = pres_raw / 4.0
+        else:
+            print("error altimeter")
             
             
-        #Export data
-            storage.write(f"{t} C,{h} RH, {pressure_pa:.2f} Pa, {local_time[0]} {local_time[1]} {local_time[2]} {local_time[3]}:{local_time[4]}:{local_time[5]}\n")
-            storage.flush() 
-            #open("/sd/monitoring.csv","a").write(f"{t} C,{h} RH, {pressure_pa:.2f} Pa, {local_time[0]} {local_time[1]} {local_time[2]} {local_time[3]}:{local_time[4]}:{local_time[5]}\n")
-            #open("/sd/monitoring.csv","a").flush()
+        local_time = time.localtime()
+            
+        print(f"Temperature: {t} C, Humidity: {h} RH, Relative Pressure: {pressure_pa:.2f} Pa, Year:{local_time[0]} , Month:{local_time[1]} , Day:{local_time[2]} , Hour:{local_time[3]}, Minute:{local_time[4]}, Second:{local_time[5]}" )
+        time.sleep(1)
+        
+        
+    #Export data
+        storageI.write(f"{t} C,{h} RH, {pressure_pa:.2f} Pa, {local_time[0]} {local_time[1]} {local_time[2]} {local_time[3]}:{local_time[4]}:{local_time[5]}\n")
+        storageI.flush() 
+        storageE.write(f"{t} C,{h} RH, {pressure_pa:.2f} Pa, {local_time[0]} {local_time[1]} {local_time[2]} {local_time[3]}:{local_time[4]}:{local_time[5]}\n")
+        storageE.flush()
         
         
         
